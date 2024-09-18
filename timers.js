0dde3c7588ac109eec25c017c2d6299e0c1519e6
Dario-Gago <gagodario1@gmail.com>
2024-09-18 15:15:08 -0300

    Este son los timers de node

---
+++ timers.js
@@ -0,0 +1,9 @@
+//Este repite codigo despues de un tiempo
+setInterval(() => {
+    console.log('Press')
+}, 2000);
+
+//Este ejecuta codigo despues de un tiempo
+setTimeout(()=>{
+    console.log('Hola mundo')
+}, 1000)
